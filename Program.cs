23b9b0dd4acb36f56d472004635600657206773b
Nadya <nadya0707@yandex.ru>
2023-03-05 13:44:46 +0300

    Добавили программу

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿string[] f_array = {"hello", "world", "123", "new", "5447"};
+string[] s_array = new string[f_array.Length];
+void SecondArray(string[] f_array, string[] s_array)
+{
+    int count = 0;
+    for (int i = 0; i < f_array.Length; i++)
+    {
+    if(f_array[i].Length <= 3)
+        {
+        s_array[count] = f_array[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(f_array, s_array);
+PrintArray(s_array);
